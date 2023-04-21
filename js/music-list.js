672e9393b4d6e579346008049400b72c9427a951
Sadavarsh Saini <92135085+Sadavarsh@users.noreply.github.com>
2023-04-21 10:28:24 +0530

    Add files via upload
    Added music-player project

---
+++ js/music-list.js
@@ -0,0 +1,36 @@
+
+
+let allMusic = [
+  {
+    name: " Agar Tum Saath Ho",
+    artist: " Alka Yagnik, Arijit Singh",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "  Baatein Ye Kabhi Na",
+    artist: " Ali Fazal, Sapna Pabbi, Gurmeet Choudhary",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: " Baaton Ko Teri",
+    artist: " Arijit Singh, Himesh Reshammiya",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Enna Sona",
+    artist: "A.R. Rahman, Arijit Singh",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Hawayein",
+    artist: " Pritam, Arijit Singh",
+    img: "music-5",
+    src: "music-5"
+  },
+
+];
+
